d821b8f6d876706f75ff367a7eebc67cf7cbaa7f
jhon.hernandez <jhon.hernandez>
2025-09-06 23:14:57 -0500

    feat: add createWorker handler method and request dto

--- src/main/java/com/pragma/fc/user_service/application/handler/UserHandler.java
+++ src/main/java/com/pragma/fc/user_service/application/handler/UserHandler.java
@@ -1,37 +1,58 @@
 package com.pragma.fc.user_service.application.handler;
 
-import com.pragma.fc.user_service.application.dto.request.CreateOwnerRequestDto;
+import com.pragma.fc.user_service.application.dto.request.CreateUserRequestDto;
 import com.pragma.fc.user_service.application.dto.response.RoleResponseDto;
 import com.pragma.fc.user_service.application.dto.response.UserResponseDto;
-import com.pragma.fc.user_service.application.mapper.ICreateOwnerRequestMapper;
+import com.pragma.fc.user_service.application.mapper.ICreateUserRequestMapper;
 import com.pragma.fc.user_service.application.mapper.IRoleResponseMapper;
 import com.pragma.fc.user_service.application.mapper.IUserResponseMapper;
+import com.pragma.fc.user_service.domain.api.IAuthServicePort;
 import com.pragma.fc.user_service.domain.api.IUserServicePort;
 import com.pragma.fc.user_service.domain.model.Role;
 import com.pragma.fc.user_service.domain.model.User;
+import com.pragma.fc.user_service.infraestructure.input.security.entity.JwtAuthenticationToken;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
 @Service
 @Transactional
 @RequiredArgsConstructor
 public class UserHandler implements IUserHandler {
-    private final ICreateOwnerRequestMapper createOwnerRequestMapper;
-    private final IUserResponseMapper createOwnerResponseMapper;
+    private final ICreateUserRequestMapper createUserRequestMapper;
+    private final IUserResponseMapper userResponseMapper;
     private final IRoleResponseMapper roleResponseMapper;
     private final IUserServicePort userServicePort;
+    private final IAuthServicePort authServicePort;
 
     @Override
-    public UserResponseDto createOwner(CreateOwnerRequestDto dto) {
-        User user = createOwnerRequestMapper.toModel(dto);
+    public UserResponseDto createOwner(CreateUserRequestDto dto) {
+        User user = createUserRequestMapper.toModel(dto);
         User newOwner = userServicePort.createOwner(user);
-        return createOwnerResponseMapper.toDto(newOwner);
+        return userResponseMapper.toDto(newOwner);
     }
 
     @Override
     public RoleResponseDto getUserRole(Long documentNumber) {
         Role role = userServicePort.getUserRole(documentNumber);
         return roleResponseMapper.toDto(role);
+    }
+
+    @Override
+    public UserResponseDto createWorker(CreateUserRequestDto dto) {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        Long ownerDocumentNumber = null;
+
+        if (authentication instanceof JwtAuthenticationToken jwtAuthenticationToken) {
+            ownerDocumentNumber = Long.parseLong(authServicePort
+                    .extractSubject(jwtAuthenticationToken.getToken())
+            );
+        }
+
+        User user = createUserRequestMapper.toModel(dto);
+        User newOwner = userServicePort.createWorker(ownerDocumentNumber, user);
+        return userResponseMapper.toDto(newOwner);
     }
 }
